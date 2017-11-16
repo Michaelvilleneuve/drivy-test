6c14eb654e0b5f281913814666efb4b787a219a7
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-11-16 16:21:37 +0100

    Completed level 1

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,3 +1,20 @@
-require "json"
+Dir["./models/*.rb"].each { |file| require file }
+require 'json'
+require 'json/ext'
 
-# your code
+
+class Main
+  def initialize input
+    @data = JSON.parse File.read(input)
+    @cars = @data['cars'].map { |car| Car.new(car) }
+    @rentals = @data['rentals'].map { |rental| Rental.new(rental) }
+  end
+
+  def generate_output
+    File.write 'output.json', { rentals: @rentals.map { |r| r.pretty } }.to_json
+  end
+end
+
+
+drivy = Main.new 'data.json'
+drivy.generate_output
---
+++ backend/level1/models/car.rb
@@ -0,0 +1,14 @@
+class Car < Base
+  attr_reader :id, :price_per_day, :price_per_km
+
+  def initialize(car)
+    @id = car['id']
+    @price_per_day = car['price_per_day']
+    @price_per_km = car['price_per_km']
+    super
+  end
+
+  def self.find(a_car_id)
+    self.all_cars.find { |car| car.id == a_car_id }
+  end
+end
